0e803633d1376f9c4d3510cf923633ec2b396584
Armaan Nakhuda <95076879+Armaan4477@users.noreply.github.com>
2024-12-06 22:14:57 +0530

    invalid caret colour error fix

--- Desktop-app/preferences.py
+++ Desktop-app/preferences.py
@@ -175,11 +175,9 @@ class PreferencesApp(QWidget):
                 border: 1px solid #444;
                 border-radius: 4px;
                 padding: 5px;
-                caret-color: #00FF00;  /* Green cursor color */
             }
             QLineEdit:focus {
                 border: 2px solid #333333;  /* Dark grey border on focus */
-                caret-color: #00FF00;  /* Green cursor color on focus */
                 background-color: rgba(255, 255, 255, 0.1); /* Slightly opaque background on focus */
             }
         """)
