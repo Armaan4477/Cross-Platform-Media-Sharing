bf9cc492018f282c8a2e44274788f844432e72ec
Armaan Nakhuda <95076879+Armaan4477@users.noreply.github.com>
2024-12-07 12:54:15 +0530

    changed android version number

--- Android-app/CrossPlatform/app/build.gradle.kts
+++ Android-app/CrossPlatform/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         minSdk = 30
         targetSdk = 35
         versionCode = 1
-        versionName = "7.8.9"
+        versionName = "7.9.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
